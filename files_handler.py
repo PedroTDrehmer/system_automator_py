2e481b29ced06cc15113561fede5fbbed2bc3111
Pedro Drehmer <108018065+PedroTDrehmer@users.noreply.github.com>
2023-11-09 11:04:20 -0300

    Update files_handler.py

--- files_handler.py
+++ files_handler.py
@@ -1,3 +1,4 @@
+import time
 from PIL import ImageGrab
 import json
 import os
@@ -7,27 +8,28 @@ import shutil
 class FilesHandler:
 
     def __init__(self):
-        self.path_json = r"src/others/empresas_cnd.json"
+        self.path_json = r"src/json/empresas_dominio.json"
         self.path_download = r"C:\\Users\\Nexxo\\Downloads"
-        self.path_drive = r"G:\\Drives compartilhados\\Departamento Fiscal\\zCNDFederal"
+        self.path_drive = r"G:\\Drives compartilhados\\Departamento Fiscal\\zRoboCND"
         
-    
-    def criar_pasta(self, nome, data):
-        path_pasta = f"{self.path_drive}\\{nome}\\{data}"
+
+    def criar_pasta(self, data, nome):
+        path_pasta = f"{self.path_drive}\\{data}\\{nome}"
         if not os.path.exists(path_pasta):
             try:
                 os.makedirs(path_pasta)
-                print(f"PASTA - {nome} - CRIADA")
+                print("PASTA CRIADA")
             except FileNotFoundError:
                 pass
         else:
-            print(f"PASTA - {nome} - JÁ EXISTE")
+            print("PASTA JA EXISTE")
 
-
-    def capture_screenshot(self, nome, data):
-        path_screen = f"{self.path_drive}\\{nome}\\{data}\\CND FEDERAL.csv"
+        
+    def capture_screenshot(self, data, nome):
+        path_screen = f"{self.path_drive}\\{data}\\{nome}\\CND FEDERAL.png"
         screenshot = ImageGrab.grab()
         screenshot.save(path_screen)
+        print("SCREENSHOT")
 
 
     def ultimo_arquivo_baixado(self):
@@ -36,20 +38,21 @@ class FilesHandler:
 
         if files_download:
             ultimo_arquivo = os.path.join(self.path_download, files_download[-1])
-            print(f"ultimo_arquivo_baixado -  {ultimo_arquivo}")
+            print(f"ARQUIVO BAIXADO - {ultimo_arquivo}")
             return ultimo_arquivo
 
 
-    def mover_renomear(self, nome, data):
-        path_destino = f"{self.path_drive}\\{nome}\\{data}\\CND FEDERAL.csv"
+    def mover_renomear(self, data, nome):
+        path_destino = f"{self.path_drive}\\{data}\\{nome}\\CND FEDERAL.pdf"
         ultimo_arquivo = self.ultimo_arquivo_baixado()
-        if ultimo_arquivo != '':
+        if ultimo_arquivo != "":
             caminho_destino = path_destino
             diretorio = os.path.dirname(ultimo_arquivo)
             novo_caminho = os.path.join(diretorio, nome)
             os.rename(ultimo_arquivo, novo_caminho)
             shutil.move(novo_caminho, caminho_destino)
-            print("MoverRenomear")
+            time.sleep(1)
+            print("MOVER RENOMEAR")
 
 
     def deletar(self):
@@ -59,16 +62,17 @@ class FilesHandler:
                 shutil.rmtree(path)
             except OSError:
                 os.remove(path)
-        print("deletar - FINALIZADO")
+        time.sleep(1)
+        print("ARQUIVOS DELETADOS")
 
 
-    def alterar_valor_json(self, nome, chave, novo_status):
+    def alterar_valor_json(self, id, chave, novo_status):
         caminho_arquivo = self.path_json
-        with open(caminho_arquivo, encoding='utf-8-sig') as arquivo:
+        with open(caminho_arquivo, encoding="utf-8-sig") as arquivo:
             conteudo = json.load(arquivo)
         for empresa in conteudo:
-            if empresa["nome"] == nome:
+            if empresa["id"] == id:
                 empresa[chave] = novo_status
-        with open(caminho_arquivo, 'w') as arquivo:
+        with open(caminho_arquivo, "w") as arquivo:
             json.dump(conteudo, arquivo, indent=4)
-        print("JSON Alterado")
+        print("JSON ALTERADO")
