b72c42d5d284c8cdace655cb5c7fb005a13bb4a1
Pedro Drehmer <108018065+PedroTDrehmer@users.noreply.github.com>
2024-01-18 11:29:17 -0300

    Update variables.py

--- utils/variables.py
+++ utils/variables.py
@@ -1,11 +1,4 @@
-
-class Path:
-    PATH_JSON = r"src/json/empresas_cnd.json"
-    PATH_DOWNLOAD = r"C:\\Users\\Nexxo\\Downloads"
-    PATH_DRIVE = r"G:\\Drives compartilhados\\zRobo\\zRoboCND"
-
-
 class Paths:
-    PATH_JSON = r"src/json/empresas_sefaz_ba.json"
-    PATH_DOWNLOAD = r"C:\\Users\\Nexxo\\Downloads"
-    PATH_DRIVE = r"G:\\Drives compartilhados\\zRobo\\zRoboSefazBa"
+    PATH_JSON = r"src/json/empresas_.json"
+    PATH_DOWNLOAD = r"C:\\Users\\AAAAAAA\\Downloads"
+    PATH_DRIVE = r"G:\\Drives compartilhados\\zRobo"
